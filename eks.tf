76a2be15fa683fbfe0126f7f07da3ddc2c705268
jimyywang <118213236+jimyywang@users.noreply.github.com>
2023-03-02 21:38:34 -0800

    2nd commit

--- eks.tf
+++ eks.tf
@@ -1,9 +1,69 @@
-# # Create an EKS cluster
-# resource "aws_eks_cluster" "main" {
-#     name = "${values(var.tags)[0]}-EKS"
-#     role_arn = "arn:aws:iam::257248662189:role/aws-service-role/eks.amazonaws.com/AWSServiceRoleForAmazonEKS"
+# Create an EKS cluster
+resource  "aws_eks_cluster" "main" {
+    name = "${values(var.tags)[0]}-EKS-Cluster"
+    role_arn = aws_iam_role.eks.arn
 
-#     vpc_config {
-#       subnet_ids = [aws_subnet.private[0].id, aws_subnet.private[1].id]
-#     }
-# }
+    vpc_config {
+        subnet_ids = [aws_subnet.private[0].id, aws_subnet.private[1].id]
+    }
+
+    depends_on = [aws_iam_role.eks]
+}
+
+# Create EKS node group
+resource "aws_eks_node_group" "main" {
+    cluster_name = aws_eks_cluster.main.name
+    node_group_name = "${values(var.tags)[0]}-Node-Group"
+
+    scaling_config {
+        desired_size = 2
+        max_size = 4
+        min_size = 2
+    }
+
+    launch_template {
+      id = aws_launch_template.main.id
+      version = "$Latest"
+    }
+
+    depends_on = [aws_launch_template.main]
+    subnet_ids = [aws_subnet.private[0].id, aws_subnet.private[1].id]
+    node_role_arn = "arn:aws:iam::257248662189:role/aws-service-role/eks-nodegroup.amazonaws.com/AWSServiceRoleForAmazonEKSNodegroup"
+
+    tags = {
+        Name = "${values(var.tags)[0]}-Node-Group"
+    }
+}
+
+# Create EKS role
+resource "aws_iam_role" "eks" {
+  name = "${values(var.tags)[0]}-eks-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_vpc_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks.name
+}
--- lt-elb.tf
+++ lt-elb.tf
@@ -3,32 +3,10 @@ resource "aws_launch_template" "main" {
   name                   = "${values(var.tags)[0]}-Launch-Template"
   image_id               = "ami-0aa7d40eeae50c9a9"
   instance_type          = "t3.medium"
-  key_name               = "jimyywang-key"
   user_data              = base64encode(file("userdata.sh"))
   vpc_security_group_ids = [aws_security_group.private.id]
   tags = {
     Name = "${values(var.tags)[0]}-Launch-Template"
-  }
-}
-
-# Create auto scaling group
-resource "aws_autoscaling_group" "main" {
-  name                      = "${values(var.tags)[0]}-Autoscaling-Group"
-  max_size                  = 4
-  min_size                  = 2
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  desired_capacity          = 2
-  vpc_zone_identifier       = [aws_subnet.private[0].id, aws_subnet.private[1].id]
-
-  launch_template {
-    id = aws_launch_template.main.id
-  }
-
-  tag {
-    key                 = "Name"
-    value               = "${values(var.tags)[0]}-Instance"
-    propagate_at_launch = true
   }
 }
 
@@ -48,6 +26,31 @@ resource "aws_alb_target_group" "main" {
   port     = "80"
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 5
+    interval = 10
+    path = "/"
+  }
+}
+
+resource "aws_alb_listener_rule" "eks" {
+  listener_arn = aws_alb_listener.main.arn
+
+  action {
+    type = "forward"
+    target_group_arn = aws_alb_target_group.main.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/"]
+    }
+  }
+
+  depends_on = [aws_alb_listener.main]
 }
 
 resource "aws_alb_listener" "main" {
@@ -59,4 +62,6 @@ resource "aws_alb_listener" "main" {
     target_group_arn = aws_alb_target_group.main.arn
     type             = "forward"
   }
+
+  depends_on = [aws_alb.main]
 }
