6e0837fb06a03731cdc5932cccd20e12e3af18ec
jimyywang <118213236+jimyywang@users.noreply.github.com>
2023-03-08 07:46:43 -0800

    first commit

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<p>Hello, World!</p>'
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 
 
